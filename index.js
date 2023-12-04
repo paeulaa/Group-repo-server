ec7140a227021f500f8eba9d8f549aef784de425
jady001 <41458731+skrud4268@users.noreply.github.com>
2023-12-04 10:46:44 -0600

    take out routes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 
-const cors = require("cors");
+//const cors = require("cors");
 
 //require("dotenv").config({ path: "./config.env" });
 
 const port = process.env.PORT || 5050;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/routes"));
+//app.use(require("./routes/routes"));
 
 // Increase the payload limit, e.g., to 50mb
 //app.use(express.json({ limit: "50mb" }));
