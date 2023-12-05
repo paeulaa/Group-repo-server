005171e04d5dadde9db74b6b601dc805db21563c
jady001 <41458731+skrud4268@users.noreply.github.com>
2023-12-05 16:16:07 -0600

    put multer back

--- routes/routes.js
+++ routes/routes.js
@@ -2,11 +2,11 @@ const express = require("express");
 //const session = require("express-session");
 // const bcrypt = require('bcrypt');
 // const { ObjectId } = require("mongodb");
-// const multer = require("multer");
-// const upload = multer({
-//   dest: "uploads/",
-//   limits: { fileSize: 50 * 1024 * 1024 }, // Limit of 50MB
-// });
+const multer = require("multer");
+const upload = multer({
+  dest: "uploads/",
+  limits: { fileSize: 50 * 1024 * 1024 }, // Limit of 50MB
+});
 
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const appRouter = express.Router();
@@ -66,40 +66,42 @@ appRouter.route("/register").post(async function (req, response) {
   response.json(results);
 });
 
-appRouter.route("/settings").put(async function (req, response) {
-  let db_connect = dbo.getDb();
-  let userId = req.body.userId;
-  let userChange = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-  };
+appRouter
+  .route("/settings")
+  .put(upload.single("image"), async function (req, response) {
+    let db_connect = dbo.getDb();
+    let userId = req.body.userId;
+    let userChange = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+    };
 
-  console.log(userChange);
-  // Check if an image file was uploaded
-  if (req.file) {
-    const imagePath = req.file.path; // This is the path to the saved image file
-    userChange.imageUrl = imagePath; // Update the imageUrl field with the new image path
-  }
+    console.log(userChange);
+    // Check if an image file was uploaded
+    if (req.file) {
+      const imagePath = req.file.path; // This is the path to the saved image file
+      userChange.imageUrl = imagePath; // Update the imageUrl field with the new image path
+    }
 
-  const userIdentifier = { _id: new ObjectId(userId) }; // Using ObjectId for MongoDB
+    const userIdentifier = { _id: new ObjectId(userId) }; // Using ObjectId for MongoDB
 
-  // db_connect
-  //   .collection("user_account")
-  //   .updateOne(userIdentifier, { $set: userChange }, async function (err, res) {
-  //     if (err) {
-  //       response.status(500).send("Error updating user data: " + err.message);
-  //       return;
-  //     }
-  //     console.log("Updated data");
-  //     response.json(res);
-  //   });
-  const results = await db_connect
-    .collection("user_account")
-    .updateOne(userChange);
-  response.json(results);
-});
+    // db_connect
+    //   .collection("user_account")
+    //   .updateOne(userIdentifier, { $set: userChange }, async function (err, res) {
+    //     if (err) {
+    //       response.status(500).send("Error updating user data: " + err.message);
+    //       return;
+    //     }
+    //     console.log("Updated data");
+    //     response.json(res);
+    //   });
+    const results = await db_connect
+      .collection("user_account")
+      .updateOne(userChange);
+    response.json(results);
+  });
 
 appRouter.route("/firstuserinfo").get(async (req, response) => {
   let db_connect = dbo.getDb(); // Use the existing database connection
